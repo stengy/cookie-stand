df315209b9c19742babf7ab166094c39908fb95c
Devin G <engy100x@gmail.com>
2017-04-23 00:51:21 -0700

    found the misreference and fixed form not populating the entered new store name

--- cookieapp.js
+++ cookieapp.js
@@ -11,17 +11,18 @@ function createStore(event) {
 
   var form = event.target;
 
-  var name = form.name.value;
+  var enteredName = form.enteredName.value;
   var addMinCustomerHr = parseInt(form.addMinCustomerHr.value);
   var addMaxCustomerHr = parseInt(form.addMaxCustomerHr.value);
   var avgSaleCustomer = parseInt(form.avgSaleCustomer.value);
 
 
-  var addedStore = new Store (name, addMinCustomerHr, addMaxCustomerHr, avgSaleCustomer);
-  console.log('type of name', typeof name);
+  var addedStore = new Store (enteredName, addMinCustomerHr, addMaxCustomerHr, avgSaleCustomer);
+
   addedStore.generateCookiesPerHour();
   addedStore.generateTableRows();
 
+  form.reset();
 
 }
 
